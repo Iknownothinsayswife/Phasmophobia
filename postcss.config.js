639ee5060658b7e1bcf8d0296f474b9ad220a2fc
Iknownothinsayswife <oldfieldcarlton@gmail.com>
2025-04-28 17:08:30 +1000

    Fix build configuration for PostCSS and TailwindCSS compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,38 +1,6 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    ...process.env.NODE_ENV === 'production'
-      ? [
-        require('@fullhuman/postcss-purgecss')({
-          css: ['./assets/style.css'],
-          content: [
-            './public/**/*.html',
-            './src/**/*.vue',
-            './src/**/*.js',
-          ],
-          defaultExtractor(content) {
-            const contentWithoutStyleBlocks = content.replace(
-              /<style[^]+?<\/style>/gi,
-              '',
-            );
-            return (
-              contentWithoutStyleBlocks.match(
-                /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g,
-              ) || []
-            );
-          },
-          safelist: [
-            // Elements used from vue-tailwind
-            /button|select|input|textarea|p/,
-            /-(leave|enter|appear)(|-(to|from|active))$/,
-            /^(?!(|.*?:)cursor-move).+-move$/,
-            /^router-link(|-exact)-active$/,
-            /data-v-.*/,
-          ],
-        }),
-      ]
-      : [],
-    require('autoprefixer'),
-  ],
+  plugins: {
+    'tailwindcss': {},
+    'autoprefixer': {},
+  }
 };
